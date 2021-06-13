a07af41a235b089ca70f59a776551c3647c50724
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-06-13 20:36:49 +0300

    adding some of the cart

--- js/app.js
+++ js/app.js
@@ -1,51 +1,53 @@
 'use strict';
 
 // Cart constructor.
-const Cart = function (items) {
+// const Cart = function (items) {
   // this.items is an array of CartItem instances.
-  this.items = items;
-};
+//   this.items = items;
+// };
 
-Cart.prototype.addItem = function (image,title,price) {
+// Cart.prototype.addItem = function (image,title,price) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-  let newItem  = new CartItem(image,title,price);
-  this.items.push(newItem);
+//   let newItem  = new CartItem(image,title,price);
+//   this.items.push(newItem);
 
-};
+// };
 
-Cart.prototype.saveToLocalStorage = function () {
+// Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-  localStorage.setItem('card',JSON.stringify(this.items));
-};
+//   localStorage.setItem('card',JSON.stringify(this.items));
+// };
 
-Cart.prototype.removeItem = function (item) {
+// Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
-  this.items.splice(item,1);
-};
+//   this.items.splice(item,1);
+// };
 // Constructor for CartItem;
 // Image and title and price;
-const CartItem = function (image, title, price, quantity) {
+// const CartItem = function (image, title, price, quantity) {
+//   this.image = image;
+//   this.title = title;
+//   this.price = price;
+
+
+
+// };
+
+
+
+
+function Product(image, title, price, description) {
   this.image = image;
   this.title = title;
   this.price = price;
-  
-  this.quantity = quantity;
-
-};
-
-
-
-
-function Product(image, title, price) {
-  this.image = image;
-  this.title = title;
-  this.price = price;
+  this.description=description;
   Product.allProduct.push(this);
   console.log(this);
 }
 Product.allProduct = [];
-function generateProducts() {
+
+// function generateProducts() {
   new Product('img/hot1.jpg', 'Caffè Americano', 3.49);
   new Product('img/hot2.jpg', 'Cappuccino', 3.99);
   new Product('img/hot3.jpg', 'Espresso', 4.49);
@@ -64,6 +66,34 @@ function generateProducts() {
   new Product('img/desserts4.jpg', 'Caramel sauce', 10.99);
   new Product('img/desserts5.jpg', 'Vegan custard', 12.99);
   new Product('img/desserts6.jpg', 'Strawberry ice cream', 6.99);
+// }
+// // Calling the function
+// generateProducts();
+
+let added=[];
+// render method
+Product.prototype.render=function () {
+  // write code to generate
+  
+//   <div class="card">
+//   <img class="image" src="img/hot1.jpg" alt="Caffè Americano" style="width:100%">
+//   <h1  class="title">Caffè Americano</h1>
+//   <p class="price">$3.49</p>
+//   <p>Espresso shots topped with hot water create a light layer of crema culminating in this wonderfully
+//       rich cup with depth and nuance. Pro Tip: For an additional boost, ask your barista to try this with
+//       an extra shot.</p>
+//     <button value="Add To Cart" /></p>
+// </div>
+
+// create a button
+// add event listener
+// let object=this
+// function 
+// adds the item to the added array
+// added.push(object)
+// setting to locla storage
+
+
 }
-// Calling the function
-generateProducts();
+
+// setting to local storage function
